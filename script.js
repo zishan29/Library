9f8c0206f69b5f5fa68b2533852a95c0c6c463dd
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2023-07-14 11:59:36 +0530

    Enable book inputs.

--- script.js
+++ script.js
@@ -14,14 +14,34 @@ function addBookToLibrary(title, author, pages, readStatus) {
 
 addBookToLibrary("Can't Hurt Me", "David Gogins", 200, false);
 addBookToLibrary("How to win friends and influence people", "Dale Carneige", 300, true);
-console.log(myLibrary);
 
 const newBook = document.querySelector('#new-book');
 const form = document.querySelector(".form")
 const close = document.querySelector("#close")
+const submit = document.querySelector("#submit");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const state = document.querySelector("#status");
+
 newBook.addEventListener('click', () => {
-    form.classList.add("open-form")
+    form.classList.add("open-form");
 });
 close.addEventListener('click', () => {
-    form.classList.remove("open-form")
+    form.classList.remove("open-form");
+});
+
+submit.addEventListener('click', (event) => {
+    let t = title.value;
+    let a  = author.value;
+    let p = pages.value;
+    let s = state.value;
+    addBookToLibrary(t, a, p, s);
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    state.value = true;
+    form.classList.remove("open-form");
+    console.log(myLibrary);
+    event.preventDefault();
 })
